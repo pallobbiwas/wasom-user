1d3bcd549f3f52aac1f131de5ad56794277033f3
pallobbiswas <pallobbiswas326@gmail.com>
2022-03-18 22:08:10 +0600

    modal added done

--- src/component/ReactModal/ReactModal.js
+++ src/component/ReactModal/ReactModal.js
@@ -1,33 +1,44 @@
 import React, { useState } from "react";
 import { Button, Modal } from "react-bootstrap";
 
-const ReactModal = () => {
+const ReactModal = (props) => {
+  const { title, price, description, image } = props.details;
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
   return (
-    <>
-      <Button variant="primary" onClick={handleShow}>
-        Details
-      </Button>
+    <div className="w-75">
+      <>
+        <Button variant="primary" onClick={handleShow}>
+          Details
+        </Button>
 
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Body>
+            <div className="d-flex align-iteam-center">
+              <div>
+                <img className="w-50 img-fluid" src={image} alt="" />
+              </div>
+              <div>
+                <h3>{title}</h3>
+                <p>{description.slice(0, 20)}</p>
+                <h4>{price}</h4>
+              </div>
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    </div>
   );
 };
 
--- src/component/Users/User/User.js
+++ src/component/Users/User/User.js
@@ -3,26 +3,36 @@ import ReactModal from "../../ReactModal/ReactModal";
 import "./User.css";
 
 const User = (props) => {
-  const { SetCounter }= props;
+  const { SetCounter } = props;
   // console.log(SetCounter);
-  const { title, price, /* description, */ category, image, rating } = props.customer;
+  const { title, price, category, image, rating } =
+    props.customer;
   const { rate, count } = rating;
   return (
-    <div className=" col-md-4 cards">
-      <h2>Name: {title.slice(0, 10)}</h2>
-      <img className="w-50 img-fluid" src={image} alt="" />
+    <div className=" col-md-4 cards my-4">
       <div>
-        <button>{price} .TK</button>
-        <button>{category}</button>
+        <h2>Name: {title.slice(0, 10)}</h2>
+        <img className="w-50 img-fluid" src={image} alt="" />
+        <div>
+          <button>{price} .TK</button>
+          <button>{category}</button>
+        </div>
+        <p>
+          <span>{rate}</span>
+          {count}
+          <span></span>
+        </p>
+        <div className="row py-5">
+          <div className="col-md-6">
+            <ReactModal details = {props.customer}></ReactModal>
+          </div>
+          <div className="col-md-6">
+            <button className="btn btn-info" onClick={SetCounter}>
+              Order
+            </button>
+          </div>
+        </div>
       </div>
-      <p>
-        <span>{rate}</span>
-        {count}
-        <span></span>
-      </p>
-      <ReactModal></ReactModal>
-      <button onClick={SetCounter}>Order</button>
-      
     </div>
   );
 };
